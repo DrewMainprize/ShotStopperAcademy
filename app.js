b186eccad92b320deb40e6fb860219101c8835a5
Drew Mainprize <drewmainprize@Drews-MacBook-Pro.local>
2023-01-13 17:19:08 -0500

    Initial demo

---
+++ app.js
@@ -0,0 +1,24 @@
+var express				= require("express"), //Includes express
+	app 				= express(), // Create express app
+    bodyParser 			= require("body-parser"),
+	expressSanitizer 	= require("express-sanitizer");
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+
+
+//============== ROUTES ==============
+
+//Landing
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+
+//Listener
+const PORT = process.env.port || 3000;
+app.listen(PORT, function(){
+    console.log("JustinAmmar running on port ${ PORT }");
+});
