a0adc24108ab8f9f2ed8b728831447190cdcbb54
Drew Mainprize <drewmainprize@Drews-MacBook-Pro.local>
2023-01-27 09:26:07 -0500

    Add responsive navbar on scroll, bug fixes

--- public/javascripts/modal.js
+++
@@ -1,9 +0,0 @@
-//Modal
-var contactModal = document.getElementById('#contactModal');
-//Buttons
-var contactInput = document.getElementsByClassName('.modalOpener');
-
-contactModal.addEventListener('shown.bs.modal', function() {
-    contactInput[0].focus();
-    contactInput[1].focus();
-});
